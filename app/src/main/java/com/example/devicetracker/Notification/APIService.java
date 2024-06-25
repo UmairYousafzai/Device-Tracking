1b440ea6837b8357f78339187ba9f931994caffd
Muhammad Umair Ahmad Khan <91127248+UmairYousafzai@users.noreply.github.com>
2024-06-25 16:16:42 +0500

    final commit

--- app/src/main/java/com/example/devicetracker/Notification/APIService.java
+++ app/src/main/java/com/example/devicetracker/Notification/APIService.java
@@ -8,12 +8,6 @@ import retrofit2.http.Headers;
 import retrofit2.http.POST;
 
 public interface APIService {
-    @Headers(
-            {
-                    "Content-Type:application/json",
-                    "Authorization:key=AAAAO4iaNNE:APA91bH5eKtvcPx-VukMMpiRc6IT6SH9gZdwThK6VNsvZuoPUeEhNJPZucvDCv6Bt5TU64fYYkpdAc9C5erjUaZF8QgwRWpRTMBqOOKjOBHy-2-_QYZ9b-Cp_AVcg9mfcXe_ldfmWD6i" // Your server key refer to video for finding your server key
-            }
-    )
 
     @POST("fcm/send")
     Call<MyResponse> sendNotifcation(@Body JsonObject payLoad);
